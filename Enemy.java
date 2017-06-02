bce5f9e93af02c307079f60e80b9b756e5251716
Novakn1 <novakn1040@gmail.com>
2017-06-01 21:36:21 -0700

    Pre-Last Day commit
    Finished 3 arenas. Got Teleporters working and set up a way that will teleport the player after they kill a certain amount of enemies.

---
+++ Enemy.java
@@ -0,0 +1,131 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
+import java.util.*;
+/**
+ * Write a description of class Enemy here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Enemy  extends Character
+{
+    private int direction;
+    private int speed;
+    private int count;
+    private int pause;
+    private int delay;
+    private int animNum;
+    
+    public Enemy()
+    {
+        super();
+    }
+    public Enemy(int health, int attack, int defense){
+        super(health,attack,defense);
+    }
+    /**
+     * Do whatever the Enemy likes to to just now.
+     */
+    public void act()
+    {
+        if (pause==0){
+            getExcited(100);
+            attackPlayer(50,10);
+            
+    
+        if(findClosestPlayer()){
+            randomSpeed(2,6);
+            move(speed);
+        }else{
+            moveRandom();
+        }
+        foundItem();
+        if (getHealth()<0){
+            getWorld().removeObject(this);
+            elims++;
+        }
+    }else if (pause<0){
+        pause=0;
+    }else{
+        pause--;
+    }
+    }
+    public boolean findClosestPlayer(){
+        
+        for (int i=1; i<300; i+=10){
+            List<Player> players = getObjectsInRange(i, Player.class);
+            if (players.size()>0){
+                turnTowards(players.get(0).getX(),players.get(0).getY());
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public void attackPlayer(int range, int rate)
+    {
+       List<Player> close = getObjectsInRange(range, Player.class);
+       if (close.size()>0){
+           if(count==0){
+               for (Player player: close){
+                   player.setHealth(player.getHealth()-(int)(getAttack()/player.getDefense()+1));
+                   getWorld().setCameraDirection(getRotation());
+                   getWorld().moveCamera(80);
+                   player.setImage("dmg.png");
+                   Greenfoot.delay(8);
+                   player.setImage("play.png");
+                   //World world= getWorld();
+                   //world.removeObject(close.get(0));
+                }
+            }
+           count++;
+           if(count>=rate){
+               count=0;
+           }
+       
+       }else{
+           count = 0;
+       }
+    }
+   public void getExcited(int range){
+        
+        GreenfootImage[] anim = {new GreenfootImage("en1_1.png"), new GreenfootImage("en1_2.png"),
+        new GreenfootImage("en1_3.png"), new GreenfootImage("en1_4.png")};
+        List<Player> players = getObjectsInRange(range, Player.class);
+        if (players.size() >0) {
+            if (delay == 0) {
+                setImage(anim[animNum]);
+                animNum++;
+            }
+            delay++;
+            if (delay>=4){
+                delay=0;
+            }
+        }else{
+            setImage(anim[0]);
+            delay=0;
+            animNum=0;
+        }
+        if (animNum > 3) {
+            animNum = 0;
+        }
+    }
+    public void setPause(int time)
+    {
+        pause=time;
+    }
+    public void moveRandom()
+    {
+        setRotation(getRotation()+(int)(Math.random()*10-5));
+        randomSpeed(0,1);
+        move(speed);
+    }
+    public void randomSpeed(int min,int max){
+        if (speed>=max){
+            speed=max-(int)(Math.random()*2);
+        }else if (speed<=min){
+            speed=min+(int)(Math.random()*2);
+        }else{
+            speed=speed+(int)(Math.random()*3)-1;
+        }
+    }
+}
--- Player.java
+++ Player.java
@@ -1,31 +1,86 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import greenfoot.*; // (World, Actor, GreenfootImage, and Greenfoot)
+import java.util.*;
 /**
  * Write a description of class Player here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Player extends Collision
+public class Player extends Character
 {
-    private int hp;
-    private static final int MOVE_AMT = 5;
-    /**
-     * Act - do whatever the Player wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public Player() {
-        this.hp = 3;
-    }
-    public void act() 
+    private int timer;
+    private int count;
+    private int setloco;
+    public Player()
     {
-       followMouse();
-       move();
-       atk();
-       getHit();
-       if (this.hp <= 0) {
-           Greenfoot.stop();
+        super();
+    }
+    public Player(int health, int attack, int defense){
+        super(health,attack,defense);
+    }
+    public void act(){
+        //findClosest();
+        //move(-1);
+        followMouse(5);
+      
+        attack(100, 20);
+        
+          if(getOneTouchedObject(TeleportArena2.class) != null){
+           nextLevel();
+           
+        }  
+        if(getOneTouchedObject(TeleportArena3.class) != null){
+           nextLevel();
+        }  
+        if(getOneTouchedObject(TeleportArena4.class) != null){
+           nextLevel();
+        }  
+        if(getOneTouchedObject(TeleportSpawn.class) != null){
+           nextLevel();
+        }  
+         if(setloco==1){
+             setLocation(1000,0);
+            }
+        //spawns a teleporter in SpawnArena1 after 10 enemys are killed
+        if(elims>10){
+           getWorld().addObject(new TeleportArena2(), 1250, 1150);
         }
+        
+        //spawns a teleporter in Arena2 after 20 enemys are killed
+        if(elims>20){
+           getWorld().addObject(new TeleportArena3(), 1375, 1800);
+        }
+        
+        //spawns a teleporter in Arena3 after 30 enemys are killed
+        if(elims>30){
+           getWorld().addObject(new TeleportArena4(), 1250, 1150);
+        }
+        
+        //spawns a teleporter in Arena4 after 40 enemys are killed
+        if(elims>40){
+           getWorld().addObject(new TeleportSpawn(), 1250, 1150);
+        }
+        
+        if(Greenfoot.isKeyDown("D")){
+            supperSpeedy(100);
+        }else{
+            setImage("play.png");
+            if(timer>0){
+                timer--;
+            }
+        }
+        foundItem();
+        if (getHealth()<0){
+            getWorld().removeObject(this);
+        }
+    }
+    public void followMouse(int speed){
+        MouseInfo mouse = Greenfoot.getMouseInfo();
+        if(mouse!=null){
+            turnTowards(mouse.getX(),mouse.getY());
+            getWorld().setCameraDirection(getRotation());
+        }
+        move(speed);
     }
     public void followMouse() {
         MouseInfo mi = Greenfoot.getMouseInfo();
@@ -34,40 +89,106 @@ public class Player extends Collision
             getWorld().setCameraDirection(getRotation());
         }
     }
-    public void move() {
+    public void move(int speed) {
         if (Greenfoot.isKeyDown("w")) {
-            getWorld().moveCamera(MOVE_AMT);
+            getWorld().moveCamera(speed);
+            if(getOneTouchedObject(Wall.class) != null){
+                 getWorld().moveCamera(-speed);
+                 
+                }
+                if(getOneTouchedObject(TeleportArena2.class) != null){
+                 getWorld().moveCamera(-speed);
+                }
         }
         if (Greenfoot.isKeyDown("s")) {
-            getWorld().moveCamera(-MOVE_AMT);
+            getWorld().moveCamera(-speed);
+             if(getOneTouchedObject(Wall.class) != null){
+                 getWorld().moveCamera(speed);
+                }
+                if(getOneTouchedObject(TeleportArena2.class) != null){
+                 getWorld().moveCamera(speed);
+                }
         }
     } 
-    public boolean atk() {
+    public void findClosest(){
+        
+        for (int i=1; i<12; i++){
+            List<Enemy> close = getObjectsInRange(i, Enemy.class);
+            if (close.size()>0){
+                turnTowards(close.get(0).getX(),close.get(0).getY());
+                break;
+            }
+        }
+    }
+    public void attack(int range, int rate){
         GreenfootImage[] anim = {new GreenfootImage("atk.png"), new GreenfootImage("atk1.png"),
         new GreenfootImage("atk2.png")};
-        MouseInfo mi = Greenfoot.getMouseInfo();
-        if (mi != null) {
-            if (Greenfoot.mouseClicked(null)) {
-                for (int x = 0; x < anim.length; x++) {
-                    setImage(anim[x]);
-                    Greenfoot.delay(5);
+        if (Greenfoot.isKeyDown("a")){
+            if (count==0){
+                List<Enemy> close = getObjectsInRange(range, Enemy.class);
+                if (close.size()>0){
+                    for (int i = 0; i<close.size(); i++){
+                        close.get(i).setHealth(close.get(i).getHealth()-(int)(getAttack()/close.get(i).getDefense()+1));
+                        close.get(i).setPause(10);
+                    }
+                    /*for (int x = 0; x < anim.length; x++) {
+                        setImage(anim[x]);
+                        Greenfoot.delay(5);    
+                    }*/
                 }
-                setImage("play.png");
-                return true;
+                for (int x = 0; x < anim.length; x++) {
+                        setImage(anim[x]);
+                        Greenfoot.delay(5);    
+                    }
             }
-        }
-        return false;
-    }
-    public void getHit() {
-        Actor pl = getOneTouchedObject(enemy.class);
-        if (!atk()) {
-            if (pl != null) {
-                this.hp--;
-                getWorld().moveCamera(-80);
-                setImage("dmg.png");
-                Greenfoot.delay(8);
-                setImage("play.png");
+            count++;
+            if(count>=rate){
+                count=0;
             }
+        }else{
+            count=0;
         }
+    }    
+    public void supperSpeedy(int durration){
+        if (timer<=durration){
+            setImage(new GreenfootImage("rocket.png"));
+            move(5);
+        }else{
+            setImage("play.png");
+        }
+        timer= 0;
     }
+    public void nextLevel()
+     {
+                if (getOneTouchedObject(TeleportArena2.class) != null)
+                {
+                    ScrollWorld arena2 = new Arena2();
+                    arena2.addCameraFollower(new Player(), 0, 0);
+                    Greenfoot.setWorld(arena2);
+                    elims=0;
+                    
+                }
+                if (getOneTouchedObject(TeleportArena3.class) != null)
+                {
+                    ScrollWorld arena3 = new Arena3();
+                    arena3.addCameraFollower(new Player(), 0, 0);
+                    Greenfoot.setWorld(arena3);
+                    elims=0;
+                }
+                if (getOneTouchedObject(TeleportArena4.class) != null)
+                {
+                    ScrollWorld arena4 = new Arena4();
+                    arena4.addCameraFollower(new Player(), 0, 0);
+                    Greenfoot.setWorld(arena4);
+                    elims=0;
+                }
+                if (getOneTouchedObject(TeleportSpawn.class) != null)
+                {
+                    ScrollWorld spawn = new SpawnArena1();
+                    spawn.addCameraFollower(new Player(), 0, 0);
+                    Greenfoot.setWorld(spawn);
+                    elims=0;
+                }
+     }
+
 }
